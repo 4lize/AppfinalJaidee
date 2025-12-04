9ade0acffc04606ea1c9c272cfb1974ea38a99f3
Blawnie <blawniethesecond@gmail.com>
2025-12-04 19:14:19 +0700

    OCR+UploadToDatabase

---
+++ Jaidee/OCRViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  OCRViewModel.swift
+//  Jaidee
+//
+//  Created by Teerapat Kuanphuek on 4/12/2568 BE.
+//
+
+import SwiftUI
+
+struct OCRViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OCRViewModel()
+}
