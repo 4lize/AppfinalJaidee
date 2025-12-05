487d3a519b423707082b9ec21ccdf6b8fbd38ef8
Blawnie <blawniethesecond@gmail.com>
2025-12-06 03:51:55 +0700

    เหลือ Profile

---
+++ Jaidee/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Jaidee
+//
+//  Created by Teerapat Kuanphuek on 5/12/2568 BE.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
