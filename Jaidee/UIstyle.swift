dba499d478d88d2b5b2b77996c1605ef149e66f8
Blawnie <blawniethesecond@gmail.com>
2025-12-07 16:08:15 +0700

    Final1

---
+++ Jaidee/UIstyle.swift
@@ -0,0 +1,18 @@
+//
+//  UIstyle.swift
+//  Jaidee
+//
+//  Created by Teerapat Kuanphuek on 7/12/2568 BE.
+//
+
+import SwiftUI
+
+struct UIstyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIstyle()
+}
